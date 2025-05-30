ea5fd8368b1e042ee6350ca05d192b0597273b3c
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2025-05-30 05:25:14 +0500

    Added a couple of helpers

--- utils/gpp/_macros.pp
+++ utils/gpp/_macros.pp
@@ -167,9 +167,11 @@
 \endif
 
 \define{\SC{term}}{[\term]{.smallcaps}}
-\define{pyTime}{\exec{python ./util/_pyTime.py}}
+\define{\pyTime}{\exec{python ./util/_pyTime.py}}
+\define{\ITA{term}}{[\term]{custom-style="In-text Arabic" dir="rtl" lang="ar"}}
+
+\define{\ESC{term}}{\\term}
 
 \mode{standard}{default}
-
 
 #mode user "\\" "" "{" "}{" "}" "{" "}" "#" "@"
--- utils/gpp/misc.pp
+++ utils/gpp/misc.pp
@@ -11,5 +11,8 @@
 
 \define{\SC{term}}{[\term]{.smallcaps}}
 \define{pyTime}{\exec{python ./util/_pyTime.py}}
+\define{\ITA{term}}{[\term]{custom-style="In-text Arabic" dir="rtl" lang="ar"}}
+
+\define{\ESC{term}{\\\term}}
 
 \mode{standard}{default}
